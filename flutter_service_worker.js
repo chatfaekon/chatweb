dcd9ee9be1ed711cf80bb37c3aecb3cb62abee82
ChatFaekon <games.drze@gmail.com>
2026-01-20 19:19:07 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7b52ee552c5d7744b4af1fadbcb16972
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d892b70ec53d356d45050885ebb30fb8",
+"flutter_bootstrap.js": "dfc333c103efaa8b30346da281b91960",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
