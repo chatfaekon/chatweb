7ea9c568fd771f6cd0fe5b6213b7543b99baa92f
Rodrigo <games.drze@gmail.com>
2026-01-22 11:57:18 -0300

    Auto-deploy Web: 22/01/2026 11:57:18,59

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3192288638"
+    serviceWorkerVersion: "3461013604"
   }
 });
